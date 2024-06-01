e9affb9e7cceb393155923cbd56f373e519a5a17
Aman Kumar Singh <79279299+Jr-Einstein@users.noreply.github.com>
2024-06-01 06:11:38 +0530

    Add files via upload

---
+++ pattern1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i,j;
+    for(int i=0;i<7;i++){
+        cout<<"*";
+        for(int j=0;j<7;j++){
+            cout<<"*";
+        }
+    }
+    return 0;
+}
